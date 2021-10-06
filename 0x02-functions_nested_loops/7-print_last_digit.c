b8c7e6a4190ebbaca33c53630c7895900e322c0f
nataliazzz <nataliashljm@gmail.com>
2021-10-06 13:53:43 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: integer argument
+ * Return: last digit
+ */
+
+int print_last_digit(int n)
+{
+	if (n < 0)
+		n *= -1;
+
+       	_putchar('0' + (n % 10));
+
+	return (n % 10);
+}
