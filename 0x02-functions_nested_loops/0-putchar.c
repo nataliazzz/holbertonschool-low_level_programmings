1293bb00dd23c9bb2d16e8012bb5297473976b12
nataliazzz <nataliashljm@gmail.com>
2021-10-06 12:56:21 -0700

    .

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * main print _putchar followed by a new line
+ * main - print _putchar followed by a new line
  * Return: 0
  */
 
@@ -19,3 +19,4 @@ int main(void)
 
 	return (0);
 }
+
