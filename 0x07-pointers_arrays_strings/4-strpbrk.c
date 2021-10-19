8716f33fe3732e48b2ca505d0f534635d6d98ead
nataliazzz <nataliashljm@gmail.com>
2021-10-19 11:49:05 -0700

    a string

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,289 @@
+#include "main.h"
+
+/**
+  * _strpbrk - search a string
+  * @s: source string
+  * @accept: accepted characters
+  *
+  * Return: string since the first found character
+  */
+char *_strpbrk(char *s, char *accept)
+{
+	int a = 0, b;
+
+	while (s[a])
+	{
+		b = 0;
+
+		while (accept[b])
+		{
+			if (s[a] == accept[b])
+			{
+				s += a;
+				return (s);
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return ('\0');
+}#include "main.h"
+
+/**
+ *   * _strpbrk - search a string
+ *     * @s: source string
+ *       * @accept: accepted characters
+ *         *
+ *           * Return: string since the first found character
+ *             */
+char *_strpbrk(char *s, char *accept)
+{
+		int a = 0, b;
+
+			while (s[a])
+			{
+						b = 0;
+
+								while (accept[b])
+								{
+												if (s[a] == accept[b])
+												{
+																	s += a;
+																					return (s);
+												}
+
+															b++;
+								}
+
+										a++;
+			}
+
+				return ('\0');
+}#include "main.h"
+
+/**
+ *   * _strpbrk - search a string
+ *     * @s: source string
+ *       * @accept: accepted characters
+ *         *
+ *           * Return: string since the first found character
+ *             */
+char *_strpbrk(char *s, char *accept)
+{
+		int a = 0, b;
+
+			while (s[a])
+			{
+						b = 0;
+
+								while (accept[b])
+								{
+												if (s[a] == accept[b])
+												{
+																	s += a;
+																					return (s);
+												}
+
+															b++;
+								}
+
+										a++;
+			}
+
+				return ('\0');
+}#include "main.h"
+
+/**
+ *   * _strpbrk - search a string
+ *     * @s: source string
+ *       * @accept: accepted characters
+ *         *
+ *           * Return: string since the first found character
+ *             */
+char *_strpbrk(char *s, char *accept)
+{
+		int a = 0, b;
+
+			while (s[a])
+			{
+						b = 0;
+
+								while (accept[b])
+								{
+												if (s[a] == accept[b])
+												{
+																	s += a;
+																					return (s);
+												}
+
+															b++;
+								}
+
+										a++;
+			}
+
+				return ('\0');
+}#include "main.h"
+
+/**
+ *   * _strpbrk - search a string
+ *     * @s: source string
+ *       * @accept: accepted characters
+ *         *
+ *           * Return: string since the first found character
+ *             */
+char *_strpbrk(char *s, char *accept)
+{
+		int a = 0, b;
+
+			while (s[a])
+			{
+						b = 0;
+
+								while (accept[b])
+								{
+												if (s[a] == accept[b])
+												{
+																	s += a;
+																					return (s);
+												}
+
+															b++;
+								}
+
+										a++;
+			}
+
+				return ('\0');
+}#include "main.h"
+
+/**
+ *   * _strpbrk - search a string
+ *     * @s: source string
+ *       * @accept: accepted characters
+ *         *
+ *           * Return: string since the first found character
+ *             */
+char *_strpbrk(char *s, char *accept)
+{
+		int a = 0, b;
+
+			while (s[a])
+			{
+						b = 0;
+
+								while (accept[b])
+								{
+												if (s[a] == accept[b])
+												{
+																	s += a;
+																					return (s);
+												}
+
+															b++;
+								}
+
+										a++;
+			}
+
+				return ('\0');
+}#include "main.h"
+
+/**
+ *   * _strpbrk - search a string
+ *     * @s: source string
+ *       * @accept: accepted characters
+ *         *
+ *           * Return: string since the first found character
+ *             */
+char *_strpbrk(char *s, char *accept)
+{
+		int a = 0, b;
+
+			while (s[a])
+			{
+						b = 0;
+
+								while (accept[b])
+								{
+												if (s[a] == accept[b])
+												{
+																	s += a;
+																					return (s);
+												}
+
+															b++;
+								}
+
+										a++;
+			}
+
+				return ('\0');
+}#include "main.h"
+
+/**
+ *   * _strpbrk - search a string
+ *     * @s: source string
+ *       * @accept: accepted characters
+ *         *
+ *           * Return: string since the first found character
+ *             */
+char *_strpbrk(char *s, char *accept)
+{
+		int a = 0, b;
+
+			while (s[a])
+			{
+						b = 0;
+
+								while (accept[b])
+								{
+												if (s[a] == accept[b])
+												{
+																	s += a;
+																					return (s);
+												}
+
+															b++;
+								}
+
+										a++;
+			}
+
+				return ('\0');
+}#include "main.h"
+
+/**
+ *   * _strpbrk - search a string
+ *     * @s: source string
+ *       * @accept: accepted characters
+ *         *
+ *           * Return: string since the first found character
+ *             */
+char *_strpbrk(char *s, char *accept)
+{
+		int a = 0, b;
+
+			while (s[a])
+			{
+						b = 0;
+
+								while (accept[b])
+								{
+												if (s[a] == accept[b])
+												{
+																	s += a;
+																					return (s);
+												}
+
+															b++;
+								}
+
+										a++;
+			}
+
+				return ('\0');
+}
