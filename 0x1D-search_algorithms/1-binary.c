bf6c88ff14f7aa6a077bfed887107798692497c2
Natalia Heredia <91100868+nataliazzz@users.noreply.github.com>
2022-07-16 19:12:11 -0500

    Update 1-binary.c

--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -6,46 +6,37 @@
  * @l: left index of original array
  * @r: right index of original array
  */
-void print_arr(int *array, size_t l, size_t r)
-{
-	size_t i = 0;
-
-	printf("Searching in array: ");
-	for (i = l; i <= r; i++)
-	{
-		if (i != r)
-			printf("%d, ", array[i]);
-		else
-			printf("%d\n", array[i]);
-	}
-}
-
-/**
- * binary_search - search mids
- * @array: array of ints
- * @size: size of array
- * @value: value to search for
- * Return: index at which value's found
- */
 int binary_search(int *array, size_t size, int value)
 {
-	size_t mid = 0;
-	size_t l = 0;
-	size_t r = size - 1;
+	size_t i = 0, low = 0, mid = 0, high = size - 1;
 
-	if (!array)
+	if (array == NULL)
 		return (-1);
 
-	while (l <= r)
+	while (low <= high)
 	{
-		print_arr(array, l, r);
-		mid = (l + r) / 2;
-		if (array[mid] == value)
-			return (mid);
+		mid = (high + low) / 2;
+		i = low;
+
+		printf("Searching in array: ");
+
+		for (; i <= high; ++i)
+		{
+			if (i != low)
+				printf(", %d", array[i]);
+			else
+				printf("%d", array[i]);
+		}
+
+		printf("\n");
+
+		if (array[mid] < value)
+			low = mid + 1;
 		else if (array[mid] > value)
-			r = mid - 1;
+			high = mid - 1;
 		else
-			l = mid + 1;
+			return (mid);
 	}
+
 	return (-1);
 }
